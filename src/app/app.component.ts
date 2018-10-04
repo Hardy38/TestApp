a1fb76e5c082eef4edcebca96d902a6c77719d12
Hardy <h.tsialoungou@gmail.com>
2018-10-04 17:26:35 +0200

    - Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import {Film} from './modeles/myModeles';
 
 @Component({
     selector: 'app-root',
@@ -6,11 +9,14 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-    constructor() {
+    filmsObservable: Observable<Film[]>;
+    constructor(private httpClient: HttpClient) {
     }
 
     ngOnInit() {
+        this.filmsObservable = this.httpClient
+            .get<Film[]>('https://api.themoviedb.org/3/search/movie?api_key=384da4d1d38ad08447d757fb4629fa6b&query=Jack+Reacher')
+            .do(console.log);
     }
 
 }
