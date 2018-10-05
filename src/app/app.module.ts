a9c0022330314f70a7a9f8462566b0f588f40480
Hardy <h.tsialoungou@gmail.com>
2018-10-05 15:02:02 +0200

    - Création des modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,16 @@ import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {FilmService} from './services/film.service';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MainPageComponent } from './composants/main-page/main-page.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import { ResearchComponent } from './composants/research/research.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 /*
 
 const appRoutes: Routes = [
@@ -17,10 +27,20 @@ const appRoutes: Routes = [
 @NgModule({
     declarations: [
         AppComponent,
+        MainPageComponent,
+        ResearchComponent,
     ],
     imports: [
         BrowserModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatMenuModule,
+        MatFormFieldModule,
+        MatInputModule
+
         // RouterModule.forRoot(appRoutes)
     ],
     providers: [FilmService],
